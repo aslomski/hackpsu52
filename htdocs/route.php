dae7763738935191d860bc22df299a1731375156
Alec Slomski <ais5844@psu.edu>
2017-11-05 12:49:23 -0500

    added google maps

--- htdocs/route.php
+++ htdocs/route.php
@@ -5,11 +5,38 @@
 <head>
     <title>SurviveTheDrive</title>
     <!-- Required meta tags -->
+
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+         * element that contains the map. */
+        #map {
+            height: 100%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        #floating-panel {
+            position: absolute;
+            top: 10px;
+            left: 25%;
+            z-index: 5;
+            background-color: #fff;
+            padding: 5px;
+            border: 1px solid #999;
+            text-align: center;
+            font-family: 'Roboto','sans-serif';
+            line-height: 30px;
+            padding-left: 10px;
+        }
+    </style>
 </head>
 <body>
 
@@ -53,13 +80,79 @@
 </div>
 
 
-<div class = "container">
- <?php echo $_POST["inputDestination"]; ?>
+
+<?php echo $_POST["inputDestination"]; ?>
     <br></br>
-    <?php echo $_POST["inputStart"]; ?>
+<?php echo $_POST["inputStart"]; ?>
 
 
-</div>
+<div id="map"></div>
+<script>
+
+    var originString = <?php echo $_POST["inputDestination"]; ?>;
+    var destinationString = <?php echo $_POST["inputStart"]; ?>;
+
+    <?php
+    $descriptorspec = array(
+        0 => array("pipe", "r"),
+        1 => array("pipe", "w"),
+        2 => array("file", "error-output.txt", "a")
+    );
+
+    $s1 = $_POST["inputDestination"];
+    $s2 = $_POST["inputStart"];
+
+    $process = proc_open('python main.py ' . $s1 . ' ' . $s2, $descriptorspec, $pipes);
+
+    if (is_resource($process)) {
+        $return_value = proc_close($process);
+
+        echo "command returned $return_value\n";
+    } else {
+        echo "No resource availeble";
+    }
+    ?>
+
+function initMap() {
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            center: {lat: 37.77, lng: -122.447}
+        });
+        directionsDisplay.setMap(map);
+
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+        document.getElementById('mode').addEventListener('change', function() {
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+        });
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var selectedMode = "DRIVING"
+        directionsService.route({
+            origin: {originString},  // Haight.
+            destination: {destinationString},  // Ocean Beach.
+            // Note that Javascript allows us to access the constant
+            // using square brackets and a string value as its
+            // "property."
+            travelMode: google.maps.TravelMode[selectedMode]
+        }, function(response, status) {
+            if (status == 'OK') {
+                directionsDisplay.setDirections(response);
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+    }
+</script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlqCwcgnUScdOF-b-7_Ma5ifdF-V2HGuQ&callback=initMap">
+</script>
+
+
+
+
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
